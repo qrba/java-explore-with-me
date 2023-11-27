783fb02e5c42a98ee0c3ae77fee11f73ca8a718c
Alexey <qrbatskij@yandex.ru>
2023-11-27 17:59:00 +0300

    feat: prepare modules

---
+++ stat-svc/src/main/java/ru/practicum/statsvc/StatSvcApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.statsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatSvcApplication.class, args);
+	}
+
+}
